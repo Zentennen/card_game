2b1c95fed3d16fc1cf6648db908852cdd12ae4fc
Olof Wikmark <zentennen@gmail.com>
2022-11-16 19:07:55 +0100

    Implemented FPDF code to handle names, attributes and properties to a degree

--- src/bin/pytest.rs
+++ src/bin/pytest.rs
@@ -1,45 +1,47 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 
-use std::collections::HashMap;
+use std::os::windows::process;
+
 use extrust::*;
 use card_game::*;
 use pyo3::*;
 use pyo3::prelude::*;
-use pyo3::types::{IntoPyDict, PyFloat};
+use pyo3::types::{IntoPyDict};
 
-//                                   0    1     2     3     4    5    6
-const main_attr_pad_lr: [f64; 7] = [ 0.0, 27.7, 16.0, 11.0, 7.5, 4.0, 0.8 ];
-const main_attr_icon_w: f64 = 3.5;
-const main_attr_text_w: f64 = 5.0;
-const main_attr_h: f64 = 3.5;
-const other_attr_h: f64 = 4.0;
-const card_pad: f64 = 2.0;
-const card_outer_w: f64 = 63.0;
-const card_outer_h: f64 = 88.0;
-const card_inner_w: f64 = card_outer_w - card_pad * 2.0;
-const card_inner_h: f64 = card_outer_h - card_pad * 2.0;
-const card_name_h: f64 = 6.0;
 const page_pad_t: f64 = 5.0;
 const page_pad_l: f64 = 12.0;
 const cards_per_column: usize = 3;
 const cards_per_row: usize = 3;
 const cards_per_page: usize = cards_per_column * cards_per_row;
+const card_outer_w: f64 = 63.0;
+const card_outer_h: f64 = 88.0;
+const card_inner_w: f64 = card_outer_w - card_pad * 2.0;
+const card_inner_h: f64 = card_outer_h - card_pad;
+const card_pad: f64 = 2.0;
+const name_h: f64 = 8.0;
 const name_font_size: f64 = 9.0;
+const main_attr_icon_w: f64 = 3.6;
+const main_attr_text_pad_t: f64 = main_attr_font_size * 0.38;
+const main_attr_text_pad_l: f64 = 0.8;
+const main_attr_h: f64 = 3.6;
+const main_attr_pad_b: f64 = 1.7;
+const other_attr_h: f64 = 2.7;
+const prop_h: f64 = 2.5;
+const prop_pad: f64 = 2.3;
 const main_attr_font_size: f64 = 7.0;
 const font_size: f64 = 6.0;
-const line_spacing: f64 = 1.25;
-const num_main_attribs: usize = 6;
 const default_card_string_alloc: usize = 1000;
 
 pub struct PdfHandler<'p> {
     py: Python<'p>,
-    pdf: &'p PyAny
+    pdf: &'p PyAny,
+    image_aliases: std::collections::HashMap<&'static str, &'static str>
 }
 
 impl Drop for PdfHandler<'_> {
     fn drop(&mut self) {
-        self.output();    
+        self.output();
         println!("Deserialized successfully!");
     }
 }
@@ -48,7 +50,10 @@ impl PdfHandler<'_> {
     pub fn new<'p>(py: Python<'p>) -> PdfHandler<'p> {
         let fpdf = py.import("fpdf").unwrap();
         let pdf = fpdf.getattr("FPDF").unwrap().call0().unwrap();
-        let s = PdfHandler::<'p> { py, pdf };
+        let mut image_aliases = std::collections::HashMap::new();
+        image_aliases.insert("Test Complicated Card.png", "Test Simple Card.png");
+
+        let s = PdfHandler::<'p> { py, pdf, image_aliases };
         s.init();
         s
     }
@@ -105,6 +110,16 @@ impl PdfHandler<'_> {
         self.pdf.call_method("set_xy", (), Some(args)).unwrap();
     }
 
+    pub fn text(&self, txt: &str, x: f64, y: f64) {
+        let args = (x, y, txt);
+        self.pdf.call_method1("text", args).unwrap();
+    }
+
+    pub fn write(&self, txt: &str) {
+        let args = vec![("txt", txt)].into_py_dict(self.py);
+        self.pdf.call_method("set_xy", (), Some(args)).unwrap();
+    }
+
     pub fn cell(&self, txt: &str, w: f64, h: f64) {
         let args = (w, h, txt);
         self.pdf.call_method1("cell", args).unwrap();
@@ -128,10 +143,16 @@ impl PdfHandler<'_> {
     }
 
     pub fn image(&self, name: &str, w: f64, h: f64) {
+        let name = *self.image_aliases.get(name).unwrap_or(&name);
         let path = format!("deserialize/images/{}", name);
         let args = types::PyTuple::new(self.py, &[path]);
         let kwargs = [("w", w), ("h", h)].into_py_dict(self.py);
         self.pdf.call_method("image", args, Some(kwargs)).unwrap();
+    }
+
+    pub fn has_image(&self, name: &str) -> bool {
+        cout(name);
+        self.image_aliases.contains_key(name) || std::fs::metadata(&format!("deserialize/images/{}", name)).is_ok()
     }
 
     pub fn rect(&self, x: f64, y: f64, w: f64, h: f64) {
@@ -139,10 +160,23 @@ impl PdfHandler<'_> {
         self.pdf.call_method1("rect", args).unwrap();
     }
 
+    pub fn line(&self, x1: f64, y1: f64, x2: f64, y2: f64) {
+        let args = (x1, y1, x2, y2);
+        self.pdf.call_method1("line", args).unwrap();
+    }
+
     pub fn string_w(&self, string: &str) -> f64 {
         let args = types::PyTuple::new(self.py, &[string]);
         let w = self.pdf.call_method1("get_string_width", args).unwrap().to_object(self.py);
         w.extract(self.py).unwrap()
+    }
+
+    pub fn multi_cell_h(&self, txt: &str, w: f64, h: f64) -> f64 {
+        let args = (w, h, txt);
+        let kwargs = [("split_only", true)].into_py_dict(self.py);
+        let strings = self.pdf.call_method("multi_cell", args, Some(kwargs)).unwrap().to_object(self.py);
+        let strings: Vec<&str> = strings.extract(self.py).unwrap();
+        h * strings.len() as f64
     }
 
     pub fn output(&self) {
@@ -406,6 +440,12 @@ fn add_attr_to_string(attr: &Attribute, string: &mut String) {
 fn get_main_attr_icon_data(card: &Card) -> Vec<(&str, String)> {
     let mut attribs: Vec<(&str, String)> = Vec::new();
 
+    if let Some(val) = get_attribute_value(&card.attr, "Tribute") {
+        if val != 0.0 {
+            attribs.push(("drop.png", val.to_string()));
+        }
+    }
+
     if let Some(val) = get_attribute_value(&card.attr, "Offense") {
         if val != 0.0 {
             attribs.push(("sword.png", val.to_string()));
@@ -436,37 +476,59 @@ fn get_main_attr_icon_data(card: &Card) -> Vec<(&str, String)> {
         }
     }
 
-    if let Some(val) = get_attribute_value(&card.attr, "Tribute") {
-        if val != 0.0 {
-            attribs.push(("drop.png", val.to_string()));
-        }
-    }
-
     attribs
 }
 
+fn deserialize_prop(ph: &PdfHandler, prop: &Property, base_x: f64, y: &mut f64) {
+    let efct = process_commands(prop.efct.to_string());
+    let h = ph.multi_cell_h(&efct, card_inner_w, prop_h);
+    *y -= h;
+    ph.set_xy(base_x, *y);
+    ph.multi_cell(&efct, card_inner_w, prop_h);
+    
+    if prop.attr.len() > 0 {
+        ph.set_font_i("Helvetica", font_size);
+        
+        let mut string = String::with_capacity(default_attr_string_alloc);
+        for attr in &prop.attr {
+            add_attr_to_string(attr, &mut string);
+            string.push_str(", ");
+        }
+        string.pop();
+        string.pop();
+        let string = process_commands(string);
+        
+        let h = ph.multi_cell_h(&string, card_inner_w, prop_h);
+        *y -= h;
+        ph.set_xy(base_x, *y);
+        ph.multi_cell(&string, card_inner_w, prop_h);
+        
+        ph.set_font("Helvetica", font_size);
+    }
+
+    *y -= prop_pad;
+}
+
 fn deserialize_card(ph: &PdfHandler, card: &Card, base_x: f64, base_y: f64) {
+    //name
     let mut y = base_y;
     ph.set_xy(base_x, y);
-    ph.rect(base_x, y, card_outer_w, card_outer_h);    
-
-    //name
-    ph.set_xy(base_x, y);
     ph.set_font("Helvetica", name_font_size);
-    ph.center_cell(&card.name, card_inner_w, card_name_h);
+    ph.center_cell(&card.name, card_inner_w, name_h);
 
     //main attributes
     ph.set_font("Helvetica", main_attr_font_size);
-    y += card_name_h;
+    y += name_h;
     let main_attr_icon_data = get_main_attr_icon_data(card);
-    let w = ph.string_w(&main_attr_icon_data[main_attr_icon_data.len() - 1].1);
+    let step_w = card_inner_w / main_attr_icon_data.len() as f64;
+    let a = ph.string_w(&main_attr_icon_data[main_attr_icon_data.len() - 1].1);
+    let w = step_w * (main_attr_icon_data.len() - 1) as f64 + main_attr_icon_w + main_attr_text_pad_l + a;
+    let margin = (card_inner_w - w) / 2.0;
     for (i, (icon, val)) in main_attr_icon_data.iter().enumerate() {
-        let mut x = base_x + i as f64 * card_inner_w / main_attr_icon_data.len() as f64;
+        let x = base_x + margin + i as f64 * step_w;
         ph.set_xy(x, y);
         ph.image(icon, main_attr_icon_w, main_attr_h);
-        x += main_attr_icon_w;
-        ph.set_xy(x, y);
-        ph.cell(&val, main_attr_text_w, main_attr_h);
+        ph.text(&val, x + main_attr_icon_w + main_attr_text_pad_l, y + main_attr_text_pad_t);
     }
 
     //other attributes
@@ -482,12 +544,23 @@ fn deserialize_card(ph: &PdfHandler, card: &Card, base_x: f64, base_y: f64) {
     }
     other_attr.pop();
     other_attr.pop();
-    y += main_attr_h;
+    y += main_attr_h + main_attr_pad_b;
     ph.set_xy(base_x, y);
     ph.set_font_i("Helvetica", font_size);
     ph.center_multi_cell(&process_commands(other_attr), card_inner_w, other_attr_h);
     
-
+    //properties
+    ph.set_font("Helvetica", font_size);
+    y = base_y + card_inner_h;
+    for prop in card.pass.iter().rev() {
+        deserialize_prop(ph, prop, base_x, &mut y);
+    }
+    for prop in card.trig.iter().rev() {
+        deserialize_prop(ph, prop, base_x, &mut y);
+    }
+    for prop in card.acti.iter().rev() {
+        deserialize_prop(ph, prop, base_x, &mut y);
+    }
 }
 
 fn main() -> Maybe {
@@ -502,8 +575,20 @@ fn main() -> Maybe {
                 for c in 0..cards_per_row {
                     let i = p * cards_per_page + r * cards_per_row + c;
                     if i < num_cards {
-                        let base_x = page_pad_l as f64 + c as f64 * card_outer_w + card_pad;
-                        let base_y = page_pad_t as f64 + r as f64 * card_outer_h + card_pad;
+                        let x = page_pad_l as f64 + c as f64 * card_outer_w;
+                        let y = page_pad_t as f64 + r as f64 * card_outer_h;
+
+                        let name = &format!("{}.png", &cards[i].name);
+                        if ph.has_image(name) {
+                            ph.set_xy(x, y);
+                            ph.image(name, card_outer_w, card_outer_h);
+                        }
+                        else {
+                            ph.rect(x, y, card_outer_w, card_outer_h);
+                        }
+
+                        let base_x = x + card_pad;
+                        let base_y = y;
                         deserialize_card(&ph, &cards[i], base_x, base_y)
                     }
                     else {
